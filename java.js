e664b985a60377aa85b5f46d85f3491ac0c51ddd
Shaquille Blackwood <shaquille.blackwood95@gmail.com>
2023-05-05 14:28:45 -0500

    clean-up and css addition

--- java.js
+++ java.js
@@ -1,29 +1,3 @@
-// calculate something: Calculate hypothenuse of triangle
-// decides something: decide if a person qualifies for something
-// count something: count down from 10
-
-// hypothenuse
-// c = sqrt(a^2 + b^2)
-
-// let a;
-// let b;
-//
-// document.getElementById('button').onclick = function(){
-//
-//   a = document.getElementById('sideA').value;
-//   // console.log(a);
-//   a = Number(a);
-//   //
-//   // console.log(a);
-//
-//   b = document.getElementById('sideB').value;
-//   b = Number(b);
-//
-//   c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-//
-//   document.getElementById('sideC').innerHTML = "Side C: " + Math.floor(c);
-// }
-
 //Make a decision
 
 // What should I eat?
