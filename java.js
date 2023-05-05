0435a0d385bc07d8b0077f3b0c7f859836336806
Shaquille Blackwood <shaquille.blackwood95@gmail.com>
2023-05-05 14:19:40 -0500

    initial commit

---
+++ java.js
@@ -0,0 +1,31 @@
+// calculate something: Calculate hypothenuse of triangle
+// decides something: decide if a person qualifies for something
+// count something: count down from 10
+
+// hypothenuse
+// c = sqrt(a^2 + b^2)
+
+// let a;
+// let b;
+//
+// document.getElementById('button').onclick = function(){
+//
+//   a = document.getElementById('sideA').value;
+//   // console.log(a);
+//   a = Number(a);
+//   //
+//   // console.log(a);
+//
+//   b = document.getElementById('sideB').value;
+//   b = Number(b);
+//
+//   c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+//
+//   document.getElementById('sideC').innerHTML = "Side C: " + Math.floor(c);
+// }
+
+//Make a decision
+
+// What should I eat?
+// User has a number of options to choose from
+// If choice requirements are met, then user will then move on to making their order, select payment method, and place their order.
